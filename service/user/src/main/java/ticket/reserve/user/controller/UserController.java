477c045db25974f33609d7643b62a60896dfd843
yeoeol <gnsl3528@naver.com>
2025-10-09 19:37:03 +0900

    feat: 게이트 웨이 관련 공부 후 코드 리팩토링

--- service/user/src/main/java/ticket/reserve/user/controller/UserController.java
+++ service/user/src/main/java/ticket/reserve/user/controller/UserController.java
@@ -1,15 +1,50 @@
 package ticket.reserve.user.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import ticket.reserve.user.dto.UserLoginRequestDto;
+import ticket.reserve.user.dto.UserRegisterRequestDto;
+import ticket.reserve.user.service.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/")
     public String home() {
         return "user-service";
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, Long>> register(@RequestBody UserRegisterRequestDto requestDto) {
+        Long userId = userService.register(requestDto);
+
+        Map<String, Long> response = new HashMap<>();
+        response.put("userId", userId);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody UserLoginRequestDto requestDto) {
+        String token = userService.login(requestDto.username(), requestDto.password());
+
+        Map<String, String> response = new HashMap<>();
+        response.put("accessToken", token);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/info/{userId}")
+    public String get(@PathVariable Long userId) {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
---
+++ service/user/src/main/java/ticket/reserve/user/util/JwtUtil.java
@@ -0,0 +1,35 @@
+package ticket.reserve.user.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey key;
+    private final long expiration;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret,
+                   @Value("${jwt.expiration}") long expiration
+    ) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+        this.expiration = expiration;
+    }
+
+    public String generateToken(Long userId) {
+        Date now = new Date();
+        Date exp = new Date(now.getTime() + expiration);
+        return Jwts.builder()
+                .subject(String.valueOf(userId))
+                .claim("roles", java.util.List.of("USER")) // 필요시 role 추가
+                .issuedAt(now)
+                .expiration(exp)
+                .signWith(key)
+                .compact();
+    }
+}
