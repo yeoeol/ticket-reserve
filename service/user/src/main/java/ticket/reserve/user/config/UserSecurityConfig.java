deea901b41cf518ea46c2f94a7f37f79ddc77f32
yeoeol <gnsl3528@naver.com>
2025-10-10 01:55:44 +0900

    feat: gateway 단에서 JWT 검증 필터 추가

--- service/user/src/main/java/ticket/reserve/user/config/UserSecurityConfig.java
+++ service/user/src/main/java/ticket/reserve/user/config/UserSecurityConfig.java
@@ -5,17 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class UserSecurityConfig {
-
-    public static final String[] permitURI = {
-            "/users/", "/users/register", "/users/login", "/users/**"
-    };
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -25,19 +26,17 @@ public class UserSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable);
-
-        http
-                .formLogin(AbstractHttpConfigurer::disable);
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
 
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(permitURI).permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
-//                .sessionManagement(session -> session
-//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
         ;
 
         return http.build();
