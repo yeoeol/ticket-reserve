23dc535995798246d8e9a5c04d8c1d692b8ccc3f
yeoeol <gnsl3528@naver.com>
2025-10-14 22:03:45 +0900

    feat: inventory 생성 시 남은 좌석 수 수정

--- service/inventory/src/main/java/ticket/reserve/inventory/dto/InventoryRequestDto.java
+++ service/inventory/src/main/java/ticket/reserve/inventory/dto/InventoryRequestDto.java
@@ -10,6 +10,7 @@ public record InventoryRequestDto(
         return Inventory.builder()
                 .eventId(this.eventId)
                 .totalSeats(this.totalSeats)
+                .availableSeats(this.totalSeats)
                 .build();
     }
 }
