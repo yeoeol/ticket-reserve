c2fd44448037d25a2258e1face8f15ce6c364b20
yeoeol <gnsl3528@naver.com>
2025-10-14 18:46:28 +0900

    feat: InventoryService 작성

---
+++ service/inventory/src/main/java/ticket/reserve/inventory/dto/InventoryRequestDto.java
@@ -0,0 +1,15 @@
+package ticket.reserve.inventory.dto;
+
+import ticket.reserve.inventory.domain.Inventory;
+
+public record InventoryRequestDto(
+        Long eventId,
+        int totalSeats
+) {
+    public Inventory toEntity() {
+        return Inventory.builder()
+                .eventId(this.eventId)
+                .totalSeats(this.totalSeats)
+                .build();
+    }
+}
