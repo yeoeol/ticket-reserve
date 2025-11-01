3e6e2e0436883dfe77388a72b3a506e5329d6545
yeoeol <gnsl3528@naver.com>
2025-11-01 17:49:33 +0900

    feat: PasswordEncoder 빈 수정

--- common/gateway/src/main/java/ticket/reserve/gateway/config/GatewaySecurityConfig.java
+++ common/gateway/src/main/java/ticket/reserve/gateway/config/GatewaySecurityConfig.java
@@ -2,11 +2,10 @@ package ticket.reserve.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -14,25 +13,17 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class GatewaySecurityConfig {
 
-
-
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable);
-
-        http
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable);
-
-        http
-                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable);
-
-        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .authorizeExchange(exchange -> exchange
                         .anyExchange().permitAll()
                 )
