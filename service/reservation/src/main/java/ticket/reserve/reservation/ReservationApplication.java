46ecee2aa954bcc6188273c514dcfb392aa80779
yeoeol <gnsl3528@naver.com>
2025-11-04 16:41:33 +0900

    feat: openFeign 의존성 추가

--- service/reservation/src/main/java/ticket/reserve/reservation/ReservationApplication.java
+++ service/reservation/src/main/java/ticket/reserve/reservation/ReservationApplication.java
@@ -2,8 +2,10 @@ package ticket.reserve.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReservationApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReservationApplication.class);
