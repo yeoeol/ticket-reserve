fd5605e50adf9411e1929bafdad5797d36d44ef8
yeoeol <gnsl3528@naver.com>
2025-11-04 16:41:33 +0900

    feat: openFeign 의존성 추가

--- service/inventory/src/main/java/ticket/reserve/inventory/service/InventoryService.java
+++ service/inventory/src/main/java/ticket/reserve/inventory/service/InventoryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ticket.reserve.inventory.client.EventServiceClient;
 import ticket.reserve.inventory.domain.Inventory;
+import ticket.reserve.inventory.domain.enums.InventoryStatus;
 import ticket.reserve.inventory.dto.*;
 import ticket.reserve.inventory.repository.InventoryRepository;
 
---
+++ service/reservation/src/main/java/ticket/reserve/reservation/controller/ReservationController.java
@@ -0,0 +1,7 @@
+package ticket.reserve.reservation.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ReservationController {
+}
