c5d7f9a3e7902d261cd89f49a0a5ec3f6d10bd7e
yeoeol <gnsl3528@naver.com>
2025-11-07 21:47:26 +0900

    feat: reservation-service, payment-service 결제 흐름 구성

---
+++ service/payment/src/main/java/ticket/reserve/payment/controller/PaymentController.java
@@ -0,0 +1,35 @@
+package ticket.reserve.payment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ticket.reserve.payment.service.PaymentService;
+
+import java.util.UUID;
+
+@Controller
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @GetMapping("/payments")
+    public String paymentPage(@RequestParam Long userId,
+                              @RequestParam Long reservationId,
+                              @RequestParam int amount,
+                              Model model
+    ) {
+        String orderId = UUID.randomUUID().toString().substring(0, 12);
+
+        model.addAttribute("userId", userId);
+        model.addAttribute("reservationId", reservationId);
+        model.addAttribute("amount", amount);
+        model.addAttribute("orderId", orderId);
+        model.addAttribute("orderName", "티켓 예매");
+
+        paymentService.createPayment(orderId, userId, reservationId, amount);
+        return "payment";
+    }
+}
--- service/payment/src/main/java/ticket/reserve/payment/service/PaymentService.java
+++ service/payment/src/main/java/ticket/reserve/payment/service/PaymentService.java
@@ -2,25 +2,12 @@ package ticket.reserve.payment.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ticket.reserve.payment.client.ReservationServiceClient;
-import ticket.reserve.payment.dto.PaymentConfirmRequestDto;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
 
-    private final ReservationServiceClient reservationServiceClient;
+    public void createPayment(String orderId, Long userId, Long reservationId, int amount) {
 
-    public void confirmPayment(PaymentConfirmRequestDto request) {
-        // PG사 Mock API 호출 - 결제 승인 로직
-
-        boolean paymentSuccess = true;  // 응답 결과
-
-        if (paymentSuccess) {
-            reservationServiceClient.confirmReservation(request.reservationId());
-        }
-        else {
-            reservationServiceClient.releaseReservation(request.reservationId());
-        }
     }
 }
