06eda27995d85ef8218a94cb17737243ff88ed4d
yeoeol <gnsl3528@naver.com>
2025-11-08 17:04:39 +0900

    feat: 토스 페이먼츠 연동 결제 승인 API 구현

--- service/payment/src/main/java/ticket/reserve/payment/controller/PaymentController.java
+++ service/payment/src/main/java/ticket/reserve/payment/controller/PaymentController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import ticket.reserve.payment.config.PaymentProperties;
+import ticket.reserve.payment.dto.PaymentConfirmRequestDto;
 import ticket.reserve.payment.service.PaymentService;
 
 import java.util.UUID;
@@ -34,5 +36,11 @@ public class PaymentController {
 
         paymentService.createPayment(orderId, userId, reservationId);
         return "payment";
+    }
+
+    @GetMapping("/payments/success")
+    public String paymentSuccess(@ModelAttribute PaymentConfirmRequestDto request, Model model) {
+        paymentService.confirmPayment(request);
+        return "redirect:/";
     }
 }
--- service/payment/src/main/java/ticket/reserve/payment/service/PaymentService.java
+++ service/payment/src/main/java/ticket/reserve/payment/service/PaymentService.java
@@ -3,7 +3,11 @@ package ticket.reserve.payment.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticket.reserve.payment.client.ReservationServiceClient;
+import ticket.reserve.payment.client.TossPaymentsClient;
+import ticket.reserve.payment.client.dto.TossResponseDto;
 import ticket.reserve.payment.domain.Payment;
+import ticket.reserve.payment.dto.PaymentConfirmRequestDto;
 import ticket.reserve.payment.repository.PaymentRepository;
 
 @Service
@@ -11,6 +15,8 @@ import ticket.reserve.payment.repository.PaymentRepository;
 public class PaymentService {
 
     private final PaymentRepository paymentRepository;
+    private final TossPaymentsClient tossPaymentsClient;
+    private final ReservationServiceClient reservationServiceClient;
 
     @Transactional
     public void createPayment(String orderId, Long userId, Long reservationId) {
@@ -21,5 +27,17 @@ public class PaymentService {
                 .build();
 
         paymentRepository.save(payment);
+    }
+
+    @Transactional
+    public void confirmPayment(PaymentConfirmRequestDto request) {
+        TossResponseDto tossResponseDto = tossPaymentsClient.confirmPayment(request);
+
+        Payment payment = paymentRepository.findByOrderId(tossResponseDto.orderId())
+                .orElseThrow(() -> new RuntimeException("Payment NOT FOUND : By orderId"));
+        payment.setting(tossResponseDto);
+
+        // OpenFeign - Reservation-service의 confirm 호출
+        reservationServiceClient.confirmReservation(payment.getReservationId());
     }
 }
