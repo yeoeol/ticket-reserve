88189345c9cc44ec0a1e06d096c187af41bb1363
yeoeol <gnsl3528@naver.com>
2025-11-09 22:37:49 +0900

    feat: 인증된 사용자 헤더 파싱

---
+++ service/admin/src/main/java/ticket/reserve/admin/filter/HeaderAuthenticationFilter.java
@@ -0,0 +1,32 @@
+package ticket.reserve.admin.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+public class HeaderAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String userId = request.getHeader("X-USER-ID");
+        String userRole = request.getHeader("X-User-Roles");
+
+        if (userId != null && userRole != null) {
+            List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(userRole));
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(userId, null, authorities);
+            SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
