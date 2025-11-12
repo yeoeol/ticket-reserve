f539d197862d910af30fa06956d17f5607b67b21
yeoeol <gnsl3528@naver.com>
2025-11-13 03:32:56 +0900

    refactor: inventoryService 메서드 - EventService와 의존성 일부 제거

--- service/admin/src/main/java/ticket/reserve/admin/client/inventory/InventoryServiceClient.java
+++ service/admin/src/main/java/ticket/reserve/admin/client/inventory/InventoryServiceClient.java
@@ -2,20 +2,19 @@ package ticket.reserve.admin.client.inventory;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
-import ticket.reserve.admin.client.inventory.dto.InventoryDetailResponseDto;
-import ticket.reserve.admin.client.inventory.dto.InventoryListResponseDto;
-import ticket.reserve.admin.client.inventory.dto.InventoryRequestDto;
-import ticket.reserve.admin.client.inventory.dto.InventoryUpdateRequestDto;
+import ticket.reserve.admin.client.inventory.dto.*;
+
+import java.util.List;
 
 @FeignClient(name = "INVENTORY-SERVICE")
 public interface InventoryServiceClient {
 
     @GetMapping("/api/inventory/{eventId}")
-    InventoryListResponseDto getInventories(@PathVariable Long eventId);
+    List<InventoryResponseDto> getInventories(@PathVariable Long eventId);
 
     @GetMapping("/api/inventory/{eventId}/{inventoryId}")
-    InventoryDetailResponseDto getInventory(@PathVariable("eventId") Long eventId,
-                                            @PathVariable("inventoryId") Long inventoryId);
+    InventoryResponseDto getInventory(@PathVariable("eventId") Long eventId,
+                                      @PathVariable("inventoryId") Long inventoryId);
 
     @PostMapping("/api/inventory")
     void createInventory(@RequestBody InventoryRequestDto request);
