bddfd6b470268cd60425586c682b24462ff7f041
yeoeol <gnsl3528@naver.com>
2025-11-17 16:35:06 +0900

    feat: JWT 헤더 -> 쿠키 방식으로 변경

--- service/user/src/main/java/ticket/reserve/user/service/UserService.java
+++ service/user/src/main/java/ticket/reserve/user/service/UserService.java
@@ -1,7 +1,6 @@
 package ticket.reserve.user.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
