803d0e3dd96104a9f6ec7e925a83f816b5dc649d
yeoeol <gnsl3528@naver.com>
2025-11-17 22:17:54 +0900

    refactor: DIP 원칙 준수를 위한 리팩토링

--- service/user/src/main/java/ticket/reserve/user/infrastructure/security/TokenAdapter.java
+++ service/user/src/main/java/ticket/reserve/user/infrastructure/security/TokenAdapter.java
@@ -4,25 +4,27 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ticket.reserve.user.application.port.out.GenerateTokenPort;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
-public class JwtUtil {
+public class TokenAdapter implements GenerateTokenPort {
 
     private final SecretKey key;
     private final long expiration;
 
-    public JwtUtil(@Value("${jwt.secret}") String secret,
-                   @Value("${jwt.expiration}") long expiration
+    public TokenAdapter(@Value("${jwt.secret}") String secret,
+                        @Value("${jwt.expiration}") long expiration
     ) {
         byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
         this.key = Keys.hmacShaKeyFor(keyBytes);
         this.expiration = expiration;
     }
 
+    @Override
     public String generateToken(Long userId, String role) {
         Date now = new Date();
         Date exp = new Date(now.getTime() + expiration);
