f19da60bf70390b345f233a8a33055b86f528af8
yeoeol <gnsl3528@naver.com>
2025-11-17 15:40:26 +0900

    feat: 이벤트 시작 시간 및 종료 시간 가독성을 위한 날짜/시간 포맷팅

--- service/admin/src/main/java/ticket/reserve/admin/client/event/dto/EventDetailResponseDto.java
+++ service/admin/src/main/java/ticket/reserve/admin/client/event/dto/EventDetailResponseDto.java
@@ -1,6 +1,7 @@
 package ticket.reserve.admin.client.event.dto;
 
 import lombok.Builder;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -10,12 +11,11 @@ public record EventDetailResponseDto(
         String eventTitle,          // 공연 제목
         String description,
         String location,            // 장소
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
         LocalDateTime startTime,
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
         LocalDateTime endTime,
         int availableInventory,
         int totalSeats
 ) {
-    public EventDetailResponseDto() {
-        this(null, "", "", "", null, null, 0, 0);
-    }
 }
--- service/admin/src/main/java/ticket/reserve/admin/client/event/dto/EventRequestDto.java
+++ service/admin/src/main/java/ticket/reserve/admin/client/event/dto/EventRequestDto.java
@@ -1,17 +1,17 @@
 package ticket.reserve.admin.client.event.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.time.LocalDateTime;
 
 public record EventRequestDto(
-        String eventTitle,          // 공연 제목
-        String description,         // 상세 내용
-        String location,            // 장소
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        Long id,
+        String eventTitle,
+        String description,
+        String location,
         LocalDateTime startTime,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime endTime,
-        int totalSeats             // 총 좌석 수
+        int totalSeats
 ) {
+        public EventRequestDto() {
+                this(null, "", "", "", LocalDateTime.now(), LocalDateTime.now(), 0);
+        }
 }
