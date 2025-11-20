b4da64ad83c6751c5b7ff6df312ad088fb39058c
yeoeol <gnsl3528@naver.com>
2025-11-20 18:46:51 +0900

    feat: 관리자 셋업 리스너 추가

--- service/user/src/main/java/ticket/reserve/user/domain/role/repository/RoleRepository.java
+++ service/user/src/main/java/ticket/reserve/user/domain/role/repository/RoleRepository.java
@@ -3,5 +3,8 @@ package ticket.reserve.user.domain.role.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ticket.reserve.user.domain.role.Role;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(String roleName);
 }
