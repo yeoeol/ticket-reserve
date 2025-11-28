e8859dc83056506ecac4af77d8c09c173a366065
yeoeol <gnsl3528@naver.com>
2025-11-29 01:09:45 +0900

    feat: 대기열 시스템 스케줄링 청소 코드 추가

--- service/reservation/src/main/java/ticket/reserve/reservation/infrastucture/scheduler/ReservationScheduler.java
+++ service/reservation/src/main/java/ticket/reserve/reservation/infrastucture/scheduler/ReservationScheduler.java
@@ -2,14 +2,10 @@ package ticket.reserve.reservation.infrastucture.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ticket.reserve.reservation.application.ReservationExpiryService;
-import ticket.reserve.reservation.infrastucture.client.EventServiceClient;
-
-import java.util.List;
-import java.util.Set;
+import ticket.reserve.reservation.infrastucture.persistence.QueueService;
 
 import static java.util.concurrent.TimeUnit.*;
 
@@ -19,11 +15,7 @@ import static java.util.concurrent.TimeUnit.*;
 public class ReservationScheduler {
 
     private final ReservationExpiryService reservationExpiryService;
-    private final RedisTemplate<String, String> redisTemplate;
-    private final EventServiceClient eventServiceClient;
-
-    private static final long ALLOW_COUNT = 100L;   // 한 번에 입장시킬 인원 수 (조절 가능)
-    private static final long ACTIVE_TTL = 5*60*1000L;  // ACTIVE 상태 유지 시간 (5분 뒤 만료)
+    private final QueueService queueService;
 
     // 1분마다 실행
     @Scheduled(fixedDelay = 1L, timeUnit = MINUTES)
@@ -33,25 +25,7 @@ public class ReservationScheduler {
 
     // 3초마다 실행
     @Scheduled(fixedDelay = 3L, timeUnit = SECONDS)
-    public void enterUser() {
-        List<Long> eventIds = eventServiceClient.getEventIds();
-
-        for (Long eventId : eventIds) {
-            String waitingKey = "queue:waiting:" + eventId;
-            String activeKey = "queue:active:" + eventId;
-
-            // 대기열에서 선착순 ALLOW_COUNT 수만큼 조회
-            Set<String> usersToEnter = redisTemplate.opsForZSet().range(waitingKey, 0, ALLOW_COUNT - 1);
-            if (usersToEnter == null || usersToEnter.isEmpty()) continue;
-
-            log.info("[ReservationScheduler.enterUser] {}명 입장 처리 중...", usersToEnter.size());
-
-            // 입장열로 이동
-            long expireTime = System.currentTimeMillis() + ACTIVE_TTL;
-            for (String userId : usersToEnter) {
-                redisTemplate.opsForZSet().add(activeKey, userId, expireTime);
-                redisTemplate.opsForZSet().remove(waitingKey, userId);
-            }
-        }
+    public void processQueue() {
+        queueService.processQueue();
     }
 }
