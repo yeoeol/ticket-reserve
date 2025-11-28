de44d663ef230632bb133b1504f9ee975dd990ee
yeoeol <gnsl3528@naver.com>
2025-11-28 21:04:01 +0900

    feat: @AllowedUser 개발

---
+++ service/reservation/src/main/java/ticket/reserve/reservation/global/aop/QueueInterceptorAop.java
@@ -0,0 +1,44 @@
+package ticket.reserve.reservation.global.aop;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import ticket.reserve.global.exception.CustomException;
+import ticket.reserve.global.exception.ErrorCode;
+import ticket.reserve.reservation.global.annotation.AllowedUser;
+import ticket.reserve.reservation.global.util.CustomSpringELParser;
+import ticket.reserve.reservation.infrastucture.persistence.QueueService;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class QueueInterceptorAop {
+
+    private final QueueService queueService;
+
+    @Before(value = "@annotation(ticket.reserve.reservation.global.annotation.AllowedUser)")
+    public void checkAllowed(final JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        String[] parameterNames = signature.getParameterNames();
+        Object[] args = joinPoint.getArgs();
+        AllowedUser allowedUser = method.getAnnotation(AllowedUser.class);
+
+        Long eventId = (Long) CustomSpringELParser.getDynamicValue(
+                parameterNames, args, allowedUser.eventId()
+        );
+        Long userId = (Long) CustomSpringELParser.getDynamicValue(
+                parameterNames, args, allowedUser.eventId()
+        );
+
+        // Active queue에 있는지 확인
+        if (!queueService.isAllowed(eventId, userId)) {
+            throw new CustomException(ErrorCode.NOT_ALLOWED);
+        }
+    }
+}
