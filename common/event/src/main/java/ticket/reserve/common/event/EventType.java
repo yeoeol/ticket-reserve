90768c74a52a79c9e634976e55f6ed37c01345c7
yeoeol <gnsl3528@naver.com>
2025-12-23 01:33:54 +0900

    feat: 공통 이벤트 모듈 클래스 생성

--- common/event/src/main/java/ticket/reserve/common/event/EventType.java
+++ common/event/src/main/java/ticket/reserve/common/event/EventType.java
@@ -2,10 +2,12 @@ package ticket.reserve.common.event;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ticket.reserve.common.event.payload.EventCreatedEventPayload;
 import ticket.reserve.common.event.payload.PaymentConfirmedEventPayload;
 import ticket.reserve.common.event.payload.ReservationExpiredPayload;
 
+@Slf4j
 @Getter
 @RequiredArgsConstructor
 public enum EventType {
@@ -15,9 +17,17 @@ public enum EventType {
     RESERVATION_EXPIRED(ReservationExpiredPayload.class, Topic.TICKET_RESERVE_RESERVATION)
     ;
 
-
     private final Class<? extends EventPayload> payloadClass;
     private final String topic;
+
+    public static EventType from(String type) {
+        try {
+            return valueOf(type);
+        } catch (Exception e) {
+            log.error("[EventType.from] type={}", type, e);
+            return null;
+        }
+    }
 
     public static class Topic {
         public static final String TICKET_RESERVE_ADMIN = "ticket-reserve-admin";
