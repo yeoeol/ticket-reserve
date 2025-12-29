1d2cb2bf9b4c67b71a15bf120f6a4e6a2cd791f6
yeoeol <gnsl3528@naver.com>
2025-12-29 22:49:50 +0900

    refactor: default_batch_fetch_size 설정으로 N+1문제 해결

--- service/user/src/main/java/ticket/reserve/user/domain/user/repository/UserRepository.java
+++ service/user/src/main/java/ticket/reserve/user/domain/user/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package ticket.reserve.user.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ticket.reserve.user.domain.user.User;
 
 import java.util.Optional;
@@ -10,10 +8,20 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
+    Optional<User> findByEmail(String email);
+
+    /**
+     * N+1 제거를 위한 페치 조인 쿼리 -> yml의 Batch Size 설정을 통해 IN절을 사용하는 것으로 대체
+     *
     @Query("SELECT u " +
             "FROM User u join fetch u.userRoles ur join fetch ur.role r " +
             "WHERE u.username=:username")
     Optional<User> findByUsernameWithRoles(@Param("username") String username);
 
-    Optional<User> findByEmail(String email);
+    @Query("SELECT u " +
+           "FROM User u " +
+           "   JOIN FETCH u.userRoles ur " +
+           "   JOIN FETCH ur.role r")
+    List<User> findAllWithRoles();
+     */
 }
