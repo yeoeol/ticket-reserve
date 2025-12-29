02721a424fb328a57472827f2873a1ba3abc9f62
yeoeol <gnsl3528@naver.com>
2025-12-30 01:06:32 +0900

    refactor: 정적 파일 (css) 분리 및 게이트웨이 화이트리스트 추가

---
+++ service/user/src/main/java/ticket/reserve/user/infrastructure/config/WebConfig.java
@@ -0,0 +1,15 @@
+package ticket.reserve.user.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/users/css/**")
+                .addResourceLocations("classpath:/static/css/");
+    }
+}
