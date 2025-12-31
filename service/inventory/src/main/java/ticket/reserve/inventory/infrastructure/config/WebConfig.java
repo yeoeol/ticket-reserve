4641cc152b7c88314162483b7372c4f8cb635664
yeoeol <gnsl3528@naver.com>
2026-01-01 02:15:17 +0900

    refactor: inventory css 파일 분리

---
+++ service/inventory/src/main/java/ticket/reserve/inventory/infrastructure/config/WebConfig.java
@@ -0,0 +1,15 @@
+package ticket.reserve.inventory.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/inventories/css/**")
+                .addResourceLocations("classpath:/static/css/");
+    }
+}
