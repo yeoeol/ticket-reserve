4ba626f79e477f3d410a38e71b20f52e537d66cd
yeoeol <gnsl3528@naver.com>
2026-01-03 16:50:13 +0900

    refactor: Admin-EventService 분리

--- service/admin/src/main/java/ticket/reserve/admin/application/AdminService.java
+++ service/admin/src/main/java/ticket/reserve/admin/application/AdminService.java
@@ -2,48 +2,18 @@ package ticket.reserve.admin.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ticket.reserve.admin.application.port.out.EventPort;
 import ticket.reserve.admin.application.port.out.InventoryPort;
-import ticket.reserve.admin.application.dto.event.request.EventRequestDto;
-import ticket.reserve.admin.application.dto.event.request.EventUpdateRequestDto;
-import ticket.reserve.admin.application.dto.event.response.EventDetailResponseDto;
-import ticket.reserve.admin.application.dto.event.response.EventResponseDto;
 import ticket.reserve.admin.application.dto.inventory.request.InventoryRequestDto;
 import ticket.reserve.admin.application.dto.inventory.request.InventoryUpdateRequestDto;
 import ticket.reserve.admin.application.dto.inventory.response.InventoryResponseDto;
 import ticket.reserve.admin.application.dto.inventory.response.InventoryListPageDto;
 
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
 
-    private final EventPort eventPort;
     private final InventoryPort inventoryPort;
-
-    /**
-     * EVENT-SERVICE
-     */
-    public List<EventResponseDto> getEvents() {
-        return eventPort.getEvents();
-    }
-
-    public EventDetailResponseDto getEvent(Long eventId) {
-        return eventPort.getEvent(eventId);
-    }
-
-    public EventDetailResponseDto createEvent(EventRequestDto request) {
-        return eventPort.createEvent(request);
-    }
-
-    public void updateEvent(Long eventId, EventUpdateRequestDto request) {
-        eventPort.updateEvent(eventId, request);
-    }
-
-    public void deleteEvent(Long eventId) {
-        eventPort.deleteEvent(eventId);
-    }
 
     /**
      * INVENTORY-SERVICE
