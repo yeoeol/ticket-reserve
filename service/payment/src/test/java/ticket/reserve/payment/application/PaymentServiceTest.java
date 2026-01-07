0e1596e9a436059dff3c4e8eac99f309ebc55083
yeoeol <gnsl3528@naver.com>
2026-01-07 16:54:33 +0900

    test: 결제 승인 성공 테스트 코드 작성

--- service/payment/src/test/java/ticket/reserve/payment/application/PaymentServiceTest.java
+++ service/payment/src/test/java/ticket/reserve/payment/application/PaymentServiceTest.java
@@ -1,19 +1,30 @@
 package ticket.reserve.payment.application;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ticket.reserve.common.event.EventType;
+import ticket.reserve.common.event.payload.PaymentConfirmedEventPayload;
 import ticket.reserve.common.outboxmessagerelay.OutboxEventPublisher;
+import ticket.reserve.payment.application.dto.request.PaymentConfirmRequestDto;
+import ticket.reserve.payment.application.dto.response.TossResponseDto;
 import ticket.reserve.payment.application.port.out.TossPaymentsPort;
 import ticket.reserve.payment.domain.Payment;
 import ticket.reserve.payment.domain.repository.PaymentRepository;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -31,7 +42,7 @@ class PaymentServiceTest {
     @DisplayName("결제 생성 성공 - 파라미터 정보를 바탕으로 결제 엔티티가 생성된다")
     void createPaymentSuccess() {
         //given
-        Payment payment = createPayment(1L, 1L, 10L);
+        Payment payment = createPayment(1L, 1L, 1L, 10L);
 
         //when
         paymentService.createPayment(payment.getOrderId(), 1L, 1L, 10L);
@@ -40,10 +51,42 @@ class PaymentServiceTest {
         verify(paymentRepository, times(1)).save(any(Payment.class));
     }
 
+    @Test
+    @DisplayName("결제 승인 성공 - 토스 페이먼츠 API를 호출 후 받은 응답 값으로, 이전에 생성했던 결제 엔티티의 필드를 채운다")
+    void confirmPaymentSuccess() {
+        //given
+        Payment payment = createPayment(1L, 1L, 1L, 10L);
 
-    private Payment createPayment(Long userId, Long reservationId, Long inventoryId) {
+        TossResponseDto tossResponseDto = new TossResponseDto(
+                "testPaymentKey", "DONE", payment.getOrderId(), "testOrderName",
+                OffsetDateTime.now(), OffsetDateTime.now().plusMinutes(1), 5000, "creditCard"
+        );
+        PaymentConfirmRequestDto request = new PaymentConfirmRequestDto(
+                payment.getOrderId(), tossResponseDto.paymentKey(), payment.getTotalAmount()
+        );
+
+        given(tossPaymentsPort.confirmPayment(any())).willReturn(tossResponseDto);
+        given(paymentRepository.findByOrderId(payment.getOrderId())).willReturn(Optional.of(payment));
+
+        //when
+        paymentService.confirmPayment(request);
+
+        //then
+        assertThat(payment.getPaymentKey()).isEqualTo(tossResponseDto.paymentKey());
+        assertThat(payment.getOrderName()).isEqualTo(tossResponseDto.orderName());
+        assertThat(payment.getStatus()).isEqualTo(tossResponseDto.status());
+        assertThat(payment.getRequestedAt()).isEqualTo(tossResponseDto.requestedAt().toLocalDateTime());
+        assertThat(payment.getApprovedAt()).isEqualTo(tossResponseDto.approvedAt().toLocalDateTime());
+        assertThat(payment.getTotalAmount()).isEqualTo(tossResponseDto.totalAmount());
+
+        verify(outboxEventPublisher, times(1))
+                .publish(eq(EventType.PAYMENT_CONFIRMED), any(PaymentConfirmedEventPayload.class), anyLong());
+    }
+
+    private Payment createPayment(Long paymentId, Long userId, Long reservationId, Long inventoryId) {
         String orderId = UUID.randomUUID().toString().substring(0, 12);
         return Payment.builder()
+                .id(paymentId)
                 .userId(userId)
                 .reservationId(reservationId)
                 .inventoryId(inventoryId)
