23f54c15e0bf4365248321c3a0b4d28d12bf2b98
yeoeol <gnsl3528@naver.com>
2026-01-08 22:55:44 +0900

    feat: Payment 관련 검증 기능 구현

--- service/payment/src/main/java/ticket/reserve/payment/application/dto/request/PaymentConfirmRequestDto.java
+++ service/payment/src/main/java/ticket/reserve/payment/application/dto/request/PaymentConfirmRequestDto.java
@@ -1,8 +1,16 @@
 package ticket.reserve.payment.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public record PaymentConfirmRequestDto(
+        @NotBlank(message = "주문ID는 필수입니다.")
         String orderId,
+        @NotBlank(message = "결제 키는 필수입니다.")
         String paymentKey,
-        int amount
+        @NotNull(message = "가격은 필수입니다.")
+        @PositiveOrZero(message = "가격은 0원 이상이어야 합니다.")
+        Integer amount
 ) {
 }
--- service/payment/src/main/java/ticket/reserve/payment/application/dto/request/PaymentPageRequest.java
+++ service/payment/src/main/java/ticket/reserve/payment/application/dto/request/PaymentPageRequest.java
@@ -1,9 +1,17 @@
 package ticket.reserve.payment.application.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public record PaymentPageRequest(
+        @NotNull(message = "사용자ID는 필수입니다.")
         Long userId,
+        @NotNull(message = "예매ID는 필수입니다.")
         Long reservationId,
+        @NotNull(message = "좌석ID는 필수입니다.")
         Long inventoryId,
-        int amount
+        @NotNull(message = "가격은 필수입니다.")
+        @PositiveOrZero(message = "가격은 0원 이상이어야 합니다.")
+        Integer amount
 ) {
 }
