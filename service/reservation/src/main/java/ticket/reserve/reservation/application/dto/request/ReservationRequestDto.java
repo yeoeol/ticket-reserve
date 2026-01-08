2d6686934fdb15ab2dddb9610419a7b18a482351
yeoeol <gnsl3528@naver.com>
2026-01-08 20:01:49 +0900

    feat: 예매 요청 검증 기능 구현

--- service/reservation/src/main/java/ticket/reserve/reservation/application/dto/request/ReservationRequestDto.java
+++ service/reservation/src/main/java/ticket/reserve/reservation/application/dto/request/ReservationRequestDto.java
@@ -1,12 +1,18 @@
 package ticket.reserve.reservation.application.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import ticket.reserve.reservation.domain.Reservation;
 import ticket.reserve.reservation.domain.enums.ReservationStatus;
 
 public record ReservationRequestDto(
+        @NotNull(message = "이벤트ID는 필수입니다.")
         Long eventId,
+        @NotNull(message = "좌석ID는 필수입니다.")
         Long inventoryId,
-        int price
+        @NotNull(message = "가격은 필수입니다.")
+        @PositiveOrZero(message = "가격은 0원 이상이어야 합니다.")
+        Integer price
 ) {
     public Reservation toEntity(Long userId) {
         return Reservation.builder()
