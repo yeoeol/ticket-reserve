b928e2881f3908c978bb2de172eda1f65b9dd1ed
yeoeol <gnsl3528@naver.com>
2026-01-11 00:01:17 +0900

    feat: TSID 자동 설정 파일 추가

--- service/inventory/src/main/java/ticket/reserve/inventory/infrastructure/config/TsidConfig.java
+++
@@ -1,9 +0,0 @@
-package ticket.reserve.inventory.infrastructure.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "ticket.reserve.tsid")
-public class TsidConfig {
-}
