30066c5392816a7065d4652975bd5994a6a272c7
yeoeol <gnsl3528@naver.com>
2026-01-18 14:58:05 +0900

    refactor: service:event 삭제

--- service/event/src/main/java/ticket/reserve/busking/application/port/out/ImagePort.java
+++
@@ -1,8 +0,0 @@
-package ticket.reserve.busking.application.port.out;
-
-import org.springframework.web.multipart.MultipartFile;
-import ticket.reserve.busking.application.dto.response.ImageResponseDto;
-
-public interface ImagePort {
-    ImageResponseDto uploadImage(MultipartFile file);
-}
