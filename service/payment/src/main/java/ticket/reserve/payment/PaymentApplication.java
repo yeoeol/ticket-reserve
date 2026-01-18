0e22aea6791b54283ce7803d258dc07a8d573ef5
yeoeol <gnsl3528@naver.com>
2026-01-18 14:57:12 +0900

    feat: payment-service 패키지 스캔 수정

--- service/payment/src/main/java/ticket/reserve/payment/PaymentApplication.java
+++ service/payment/src/main/java/ticket/reserve/payment/PaymentApplication.java
@@ -2,13 +2,9 @@ package ticket.reserve.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
-@EntityScan(basePackages = {"ticket.reserve"})
-@EnableJpaRepositories(basePackages = "ticket.reserve")
 @SpringBootApplication
 public class PaymentApplication {
     public static void main(String[] args) {
---
+++ service/payment/src/main/java/ticket/reserve/payment/infrastructure/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package ticket.reserve.payment.infrastructure.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EntityScan(basePackages = {"ticket.reserve"})
+@EnableJpaRepositories(basePackages = "ticket.reserve")
+public class JpaConfig {
+}
