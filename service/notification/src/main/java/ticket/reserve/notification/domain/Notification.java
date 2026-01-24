c1287d93990ffe7266d4b1ff7feaa5ac3ce389aa
yeoeol <gnsl3528@naver.com>
2026-01-24 15:18:52 +0900

    feat: notification 모듈 생성 및 엔티티 생성

---
+++ service/notification/src/main/java/ticket/reserve/notification/domain/Notification.java
@@ -0,0 +1,27 @@
+package ticket.reserve.notification.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "notifications")
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "notification_id")
+    private Long id;
+
+    private String title;
+    private String content;
+
+    private Long buskingId;
+
+    private Long receiverId;
+}
