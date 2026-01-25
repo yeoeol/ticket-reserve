a475cb3e2928b90a887c2e13c08f00e6988b6def
yeoeol <gnsl3528@naver.com>
2026-01-25 21:15:04 +0900

    feat: 알림 요청 DTO 검증 기능 구현

--- service/notification/src/main/java/ticket/reserve/notification/application/dto/request/NotificationRequestDto.java
+++ service/notification/src/main/java/ticket/reserve/notification/application/dto/request/NotificationRequestDto.java
@@ -1,14 +1,20 @@
 package ticket.reserve.notification.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import ticket.reserve.core.tsid.IdGenerator;
 import ticket.reserve.notification.domain.notification.Notification;
 
 @Builder
 public record NotificationRequestDto(
+        @NotBlank(message = "알림 제목은 필수입니다.")
         String title,
+        @NotBlank(message = "알림 내용은 필수입니다.")
         String message,
+        @NotNull(message = "버스킹ID는 필수입니다.")
         Long buskingId,
+        @NotNull(message = "사용자ID는 필수입니다.")
         Long receiverId,
         int retryCount
 ) {
