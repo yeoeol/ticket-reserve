33e147c078d189f85646d778aaa8a89ed68b7b85
yeoeol <gnsl3528@naver.com>
2026-01-25 21:56:37 +0900

    feat: notification api controller 구현

---
+++ service/notification/src/main/java/ticket/reserve/notification/presentation/api/FcmApiController.java
@@ -0,0 +1,25 @@
+package ticket.reserve.notification.presentation.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ticket.reserve.notification.application.FcmTokenService;
+import ticket.reserve.notification.application.dto.request.FcmTokenRequestDto;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/fcm")
+public class FcmApiController {
+
+    private final FcmTokenService fcmTokenService;
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody FcmTokenRequestDto request) {
+        fcmTokenService.save(request);
+        return ResponseEntity.noContent().build();
+    }
+
+}
