dc9136692a05352a92683d0ea84985a00610db91
yeoeol <gnsl3528@naver.com>
2026-01-29 03:02:02 +0900

    feat: payment - 메시지 공통화

--- service/payment/src/main/java/ticket/reserve/payment/application/dto/request/PaymentConfirmRequestDto.java
+++ service/payment/src/main/java/ticket/reserve/payment/application/dto/request/PaymentConfirmRequestDto.java
@@ -5,12 +5,12 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record PaymentConfirmRequestDto(
-        @NotBlank(message = "주문ID는 필수입니다.")
+        @NotBlank(message = "{payment.orderId.not_blank}")
         String orderId,
-        @NotBlank(message = "결제 키는 필수입니다.")
+        @NotBlank(message = "{payment.paymentKey.not_blank}")
         String paymentKey,
-        @NotNull(message = "가격은 필수입니다.")
-        @PositiveOrZero(message = "가격은 0원 이상이어야 합니다.")
+        @NotNull(message = "{payment.amount.not_null}")
+        @PositiveOrZero(message = "{payment.amount.range}")
         Integer amount
 ) {
 }
