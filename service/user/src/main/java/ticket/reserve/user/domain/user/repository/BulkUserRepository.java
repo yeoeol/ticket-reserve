76146617f8c0be24cd37c11f00cffad0d236eded
yeoeol <gnsl3528@naver.com>
2026-01-29 00:25:55 +0900

    chore: 배치 사이즈 수정

--- service/user/src/main/java/ticket/reserve/user/domain/user/repository/BulkUserRepository.java
+++ service/user/src/main/java/ticket/reserve/user/domain/user/repository/BulkUserRepository.java
@@ -1,14 +1,12 @@
 package ticket.reserve.user.domain.user.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.geo.Point;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ticket.reserve.user.domain.user.User;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,6 +18,9 @@ public class BulkUserRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
+    @Value("${spring.jpa.properties.hibernate.default_batch_fetch_size}")
+    private int batchSize;
+
     public int LocationBulkUpdate(List<User> users, Map<Long, Point> pointMap) {
         String sql = "INSERT INTO users (user_id, username, password, email, location, created_at, updated_at) " +
                         "VALUES (?, ?, ?, ?, ST_SRID(POINT(?, ?), 4326), ?, ?) " +
@@ -27,26 +28,20 @@ public class BulkUserRepository {
                         "UPDATE location = VALUES(location), " +
                                 "updated_at = VALUES(updated_at)";
 
-        return jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
-                    @Override
-                    public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        User user = users.get(i);
-                        Point point = pointMap.get(user.getId());
+        return jdbcTemplate.batchUpdate(sql,
+                users,
+                batchSize,
+                (ps, user) -> {
+                    Point point = pointMap.get(user.getId());
 
-                        ps.setLong(1, user.getId());
-                        ps.setString(2, user.getUsername());
-                        ps.setString(3, user.getPassword());
-                        ps.setString(4, user.getEmail());
-                        ps.setDouble(5, point.getX());
-                        ps.setDouble(6, point.getY());
-                        ps.setTimestamp(7, Timestamp.valueOf(user.getCreatedAt()));
-                        ps.setTimestamp(8, Timestamp.valueOf(LocalDateTime.now()));
-                    }
-
-                    @Override
-                    public int getBatchSize() {
-                        return users.size();
-                    }
+                    ps.setLong(1, user.getId());
+                    ps.setString(2, user.getUsername());
+                    ps.setString(3, user.getPassword());
+                    ps.setString(4, user.getEmail());
+                    ps.setDouble(5, point.getX());
+                    ps.setDouble(6, point.getY());
+                    ps.setTimestamp(7, Timestamp.valueOf(user.getCreatedAt()));
+                    ps.setTimestamp(8, Timestamp.valueOf(LocalDateTime.now()));
                 }
         ).length;
     }
