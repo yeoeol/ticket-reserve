09a5cf72e90d18f903040c0c786042492b6c3cdb
yeoeol <gnsl3528@naver.com>
2026-01-30 13:21:43 +0900

    subscription module create

---
+++ service/subscription/src/main/java/ticket/reserve/subscription/SubscriptionApplication.java
@@ -0,0 +1,13 @@
+package ticket.reserve.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SubscriptionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionApplication.class, args);
+    }
+}
--- service/subscription/src/main/java/ticket/reserve/subscription/infrastructure/config/RestClientConfig.java
+++ service/subscription/src/main/java/ticket/reserve/subscription/infrastructure/config/RestClientConfig.java
@@ -1,9 +1,9 @@
-package ticket.reserve.notification.infrastructure.config;
+package ticket.reserve.subscription.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
-import ticket.reserve.notification.infrastructure.client.interceptor.AuthenticationInterceptor;
+import ticket.reserve.subscription.infrastructure.client.interceptor.AuthenticationInterceptor;
 
 @Configuration
 public class RestClientConfig {
