e21c868c881da10e3a4a33e8f3e8985df6613e51
yeoeol <gnsl3528@naver.com>
2026-02-03 18:32:38 +0900

    fix: busking 레이지 로딩 오류 수정

--- service/busking/src/main/java/ticket/reserve/busking/domain/busking/repository/BuskingRepository.java
+++ service/busking/src/main/java/ticket/reserve/busking/domain/busking/repository/BuskingRepository.java
@@ -5,8 +5,15 @@ import org.springframework.data.jpa.repository.Query;
 import ticket.reserve.busking.domain.busking.Busking;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BuskingRepository extends JpaRepository<Busking, Long> {
     @Query(value = "SELECT b.id FROM Busking b")
     List<Long> findIds();
+
+    @Query(value =
+            "SELECT b FROM Busking b " +
+            "LEFT JOIN FETCH b.buskingImages bi " +
+            "WHERE b.id = :id")
+    Optional<Busking> findByIdWithImage(Long id);
 }
