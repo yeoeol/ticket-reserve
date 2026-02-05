27e39331868e6fd9508bac0a91eadd80c40b003f
yeoeol <gnsl3528@naver.com>
2026-02-06 00:49:57 +0900

    feat: 스케줄러 트랜잭션 어노테이션 추가

---
+++ service/subscription/src/main/java/ticket/reserve/subscription/application/NotificationPublishService.java
@@ -0,0 +1,30 @@
+package ticket.reserve.subscription.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ticket.reserve.core.event.EventType;
+import ticket.reserve.core.event.payload.SubscriptionNotificationSentEventPayload;
+import ticket.reserve.core.outboxmessagerelay.OutboxEventPublisher;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationPublishService {
+
+    private final OutboxEventPublisher outboxEventPublisher;
+
+    @Transactional
+    public void publishNotificationEvent(Long buskingId, Set<Long> userIds, long remainingMinutes) {
+        outboxEventPublisher.publish(
+                EventType.SUBSCRIPTION_NOTIFICATION_SENT,
+                SubscriptionNotificationSentEventPayload.builder()
+                        .buskingId(buskingId)
+                        .userIds(userIds)
+                        .remainingMinutes(remainingMinutes)
+                        .build(),
+                buskingId
+        );
+    }
+}
--- service/subscription/src/main/java/ticket/reserve/subscription/infrastructure/scheduler/SubscriptionNotificationScheduler.java
+++ service/subscription/src/main/java/ticket/reserve/subscription/infrastructure/scheduler/SubscriptionNotificationScheduler.java
@@ -3,9 +3,7 @@ package ticket.reserve.subscription.infrastructure.scheduler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import ticket.reserve.core.event.EventType;
-import ticket.reserve.core.event.payload.SubscriptionNotificationSentEventPayload;
-import ticket.reserve.core.outboxmessagerelay.OutboxEventPublisher;
+import ticket.reserve.subscription.application.NotificationPublishService;
 import ticket.reserve.subscription.application.dto.response.BuskingNotificationTarget;
 import ticket.reserve.subscription.infrastructure.persistence.RedisAdapter;
 
@@ -14,40 +12,37 @@ import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import static java.lang.Math.max;
+
 @Component
 @RequiredArgsConstructor
 public class SubscriptionNotificationScheduler {
 
-    private final OutboxEventPublisher outboxEventPublisher;
+    private final NotificationPublishService notificationPublishService;
     private final RedisAdapter redisAdapter;
 
-    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.MINUTES)
+    @Scheduled(fixedRate = 10, timeUnit = TimeUnit.SECONDS)
     public void subscriptionNotificationScheduler() {
         LocalDateTime now = LocalDateTime.now();
-        LocalDateTime oneHourLater = now.plusHours(1);
 
-        Set<BuskingNotificationTarget> targets = redisAdapter.findBuskingIdsReadyToNotify(oneHourLater);
+        Set<BuskingNotificationTarget> targets = findTargets(now);
         if (targets == null || targets.isEmpty()) return;
 
         for (BuskingNotificationTarget target : targets) {
             long remainingMinutes = getRemainingMinutes(now, target.startTime());
+            if (remainingMinutes <= -10) continue;
 
             Set<Long> userIds = redisAdapter.findSubscribersByBuskingId(target.buskingId());
             if (userIds == null || userIds.isEmpty()) continue;
 
-            Long buskingId = target.buskingId();
-            outboxEventPublisher.publish(
-                    EventType.SUBSCRIPTION_NOTIFICATION_SENT,
-                    SubscriptionNotificationSentEventPayload.builder()
-                            .buskingId(buskingId)
-                            .userIds(userIds)
-                            .remainingMinutes(remainingMinutes)
-                            .build(),
-                    buskingId
-            );
-
-            redisAdapter.removeSubscriptionData(buskingId);
+            notificationPublishService.publishNotificationEvent(target.buskingId(), userIds, max(0, remainingMinutes));
         }
+    }
+
+    // 알림 대상 버스킹 정보 추출
+    private Set<BuskingNotificationTarget> findTargets(LocalDateTime now) {
+        LocalDateTime oneHourLater = now.plusHours(1);
+        return redisAdapter.findBuskingIdsReadyToNotify(oneHourLater);
     }
 
     private long getRemainingMinutes(LocalDateTime now, LocalDateTime startTime) {
