d51ce61ef2535b2aa9cc46811f93c7a05c5927d6
yeoeol <gnsl3528@naver.com>
2026-02-12 21:23:24 +0900

    refactor: LocationSyncScheduler Redis 의존성 제거

--- service/user/src/main/java/ticket/reserve/user/application/port/out/LocationPort.java
+++ service/user/src/main/java/ticket/reserve/user/application/port/out/LocationPort.java
@@ -1,5 +1,15 @@
 package ticket.reserve.user.application.port.out;
 
+import org.springframework.data.geo.Point;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public interface LocationPort {
     void addLocation(Long userId, Double latitude, Double longitude);
+
+    Set<String> findUserIds();
+
+    Map<Long, Point> getPointMapByUserIds(List<Long> userIds);
 }
--- service/user/src/main/java/ticket/reserve/user/infrastructure/persistence/LocationRedisAdapter.java
+++ service/user/src/main/java/ticket/reserve/user/infrastructure/persistence/LocationRedisAdapter.java
@@ -7,7 +7,9 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import ticket.reserve.user.application.port.out.LocationPort;
 
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -27,5 +29,33 @@ public class LocationRedisAdapter implements LocationPort {
 
         redisTemplate.opsForGeo().add(geoKey, new Point(longitude, latitude), userIdString);
         redisTemplate.opsForValue().set(activeUserKey + ":" + userIdString, "active", 30, TimeUnit.MINUTES);
+    }
+
+    @Override
+    public Set<String> findUserIds() {
+        return redisTemplate.opsForZSet().range(geoKey, 0, -1);
+    }
+
+    @Override
+    public Map<Long, Point> getPointMapByUserIds(List<Long> userIds) {
+        String[] userIdsArray = userIds.stream()
+                .map(String::valueOf)
+                .toArray(String[]::new);
+
+        List<Point> points = redisTemplate.opsForGeo().position(geoKey, userIdsArray);
+        if (points == null || points.isEmpty()) return Collections.emptyMap();
+
+        Map<Long, Point> pointMap = new HashMap<>();
+
+        for (int i = 0; i < userIds.size(); i++) {
+            Long userId = userIds.get(i);
+            Point point = points.get(i);
+
+            if (point != null) {
+                pointMap.put(userId, point);
+            }
+        }
+
+        return pointMap;
     }
 }
