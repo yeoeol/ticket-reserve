6c0cc141e1d9da95dfb45f070cd0857a5a2f23b3
yeoeol <gnsl3528@naver.com>
2026-02-14 17:31:49 +0900

    feat: 인기 버스킹 서비스 초기 설정 커밋

---
+++ service/hot-busking/src/main/java/ticket/reserve/hotbusking/application/eventhandler/EventHandlerService.java
@@ -0,0 +1,50 @@
+package ticket.reserve.hotbusking.application.eventhandler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ticket.reserve.core.event.Event;
+import ticket.reserve.core.event.EventPayload;
+import ticket.reserve.core.inbox.Inbox;
+import ticket.reserve.core.inbox.InboxRepository;
+import ticket.reserve.core.tsid.IdGenerator;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EventHandlerService {
+
+    private final IdGenerator idGenerator;
+    private final List<EventHandler> eventHandlers;
+    private final InboxRepository inboxRepository;
+
+    @Transactional
+    public void handleEvent(Event<EventPayload> event) {
+        if (inboxRepository.existsByEventId(event.getEventId())) {
+            return;
+        }
+
+        EventHandler<EventPayload> eventHandler = findEventHandler(event);
+        if (eventHandler == null) {
+            return;
+        }
+
+        try {
+            eventHandler.handle(event);
+            inboxRepository.saveAndFlush(Inbox.create(idGenerator, event.getEventId(), event.getType()));
+        } catch (DataIntegrityViolationException e) {
+            log.warn("[EventHandlerService.handleEvent] 중복 이벤트 감지: eventId={}, eventType={}", event.getEventId(), event.getType());
+        }
+    }
+
+    private EventHandler<EventPayload> findEventHandler(Event<EventPayload> event) {
+        return eventHandlers.stream()
+                .filter(eventHandler -> eventHandler.supports(event))
+                .findAny()
+                .orElse(null);
+    }
+}
---
+++ service/hot-busking/src/main/java/ticket/reserve/hotbusking/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package ticket.reserve.hotbusking.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ticket.reserve.core.global.security.filter.HeaderAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new HeaderAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+        ;
+
+        return http.build();
+    }
+}
+
