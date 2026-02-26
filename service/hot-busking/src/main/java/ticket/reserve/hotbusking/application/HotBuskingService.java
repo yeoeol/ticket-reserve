50e121f36174fd4c47ec50b9eda3bb59304b6c50
yeoeol <61906141+yeoeol@users.noreply.github.com>
2026-02-26 20:31:07 +0900

    fix: 이벤트 핸들러 순환 참조 문제 해결 (#97)

--- service/hot-busking/src/main/java/ticket/reserve/hotbusking/application/HotBuskingService.java
+++ service/hot-busking/src/main/java/ticket/reserve/hotbusking/application/HotBuskingService.java
@@ -2,16 +2,8 @@ package ticket.reserve.hotbusking.application;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-import ticket.reserve.core.event.Event;
-import ticket.reserve.core.event.EventPayload;
-import ticket.reserve.core.event.EventType;
-import ticket.reserve.core.inbox.Inbox;
-import ticket.reserve.core.inbox.InboxRepository;
-import ticket.reserve.core.tsid.IdGenerator;
 import ticket.reserve.hotbusking.application.dto.response.HotBuskingResponseDto;
-import ticket.reserve.hotbusking.application.eventhandler.EventHandler;
 import ticket.reserve.hotbusking.application.port.out.BuskingPort;
 import ticket.reserve.hotbusking.application.port.out.BuskingSubscriptionCountPort;
 import ticket.reserve.hotbusking.application.port.out.HotBuskingListPort;
@@ -24,47 +16,9 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class HotBuskingService {
 
-    private final IdGenerator idGenerator;
-    private final List<EventHandler> eventHandlers;
-    private final InboxRepository inboxRepository;
-    private final HotBuskingScoreUpdater hotBuskingScoreUpdater;
     private final HotBuskingListPort hotBuskingListPort;
     private final BuskingSubscriptionCountPort buskingSubscriptionCountPort;
     private final BuskingPort buskingPort;
-
-    public void handleEvent(Event<EventPayload> event) {
-        if (inboxRepository.existsByEventId(event.getEventId())) {
-            return;
-        }
-
-        EventHandler<EventPayload> eventHandler = findEventHandler(event);
-        if (eventHandler == null) {
-            return;
-        }
-
-
-        try {
-            if (isBuskingDeleted(event)) {
-                eventHandler.handle(event);
-            } else {
-                hotBuskingScoreUpdater.update(event, eventHandler);
-            }
-            inboxRepository.saveAndFlush(Inbox.create(idGenerator, event.getEventId(), event.getType()));
-        } catch (DataIntegrityViolationException e) {
-            log.warn("[HotBuskingService.handleEvent] 중복 이벤트 감지: eventId={}, eventType={}", event.getEventId(), event.getType());
-        }
-    }
-
-    private boolean isBuskingDeleted(Event<EventPayload> event) {
-        return EventType.BUSKING_DELETED == event.getType();
-    }
-
-    private EventHandler<EventPayload> findEventHandler(Event<EventPayload> event) {
-        return eventHandlers.stream()
-                .filter(eventHandler -> eventHandler.supports(event))
-                .findAny()
-                .orElse(null);
-    }
 
     public List<HotBuskingResponseDto> readAll() {
         return hotBuskingListPort.readAll().stream()
